a0aeb0a9d6af9968fa5f0efe47c3284469378d87
hackib-dev <hackibkenn@gmail.com>
2023-01-27 08:10:05 +0100

    Rating website

---
+++ assets/js/styles.js
@@ -0,0 +1,31 @@
+// function ratings()
+// {
+// var newRating = document.querySelector('input[name="rating"]:checked').value;
+
+// console.log(newRating)
+
+// localStorage.setItem("userInput",newRating);
+
+// window.location.href = "Thank_you.html";
+
+// }
+
+
+
+// ------------
+
+// var userInput=''
+// var ratings = document.querySelectorAll(".rate");
+
+//  for (let rating of ratings)
+//     {
+
+//       rating.addEventListener("click",function() {
+
+//         userInput = rating.value; 
+
+//         console.log(userInput);
+
+//         localStorage.setItem('selected-rating',userInput);
+//       }
+//   )}
